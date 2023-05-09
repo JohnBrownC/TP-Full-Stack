737f005a553018ec53730c18bcbdf63ac2d2c8f9
gpidote <gpidote>
2023-05-09 00:12:11 -0300

    UPDATE

--- visuals.js
+++ visuals.js
@@ -7,3 +7,41 @@ const removeAnchorFromURL = () => {
     window.history.replaceState({}, "", window.location.href.split("#")[0]);
   }, 1);
 };
+
+//show menu bar
+
+const openCloseMenubar = () => {
+  const navBar = document.getElementById("nav-menu");
+  const navBurguer = document.getElementById("hamburBtn");
+
+  console.log("display", navBar.style.display);
+  if (navBar.style.display === "") {
+    navBurguer.style.transform = "rotate(90deg)";
+    navBar.style.display = "block";
+  } else {
+    navBurguer.style.transform = "rotate(0deg)";
+    navBar.style.display = "";
+  }
+};
+
+const contactUsValidations = () => {
+ 
+  let email = document.getElementById("email");
+  let name = document.getElementById("name");
+  let message = document.getElementById("message");
+  let agree = document.getElementById("agree").checked;
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+ 
+  if (email.value == "" || name.value == "" || message.value == "") {
+    return alert("Completa todos los campos para continuar");
+  }
+  if(!email.value.match(emailRegex)){
+    return alert("Eso no parece un email")
+  }
+  if (!agree) {
+    return alert("Acepta nuestros terminos y condiciones para continuar");
+  }
+  
+
+  alert("Mensaje enviado con éxito");
+};
